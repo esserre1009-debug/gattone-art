b9c047258a657fce526d723a92a4ba7c2c684130
esserre1009-debug <stefano.romeo.pwc@gmail.com>
2026-09-19 18:02:35 +0200

    Add LinkPage route to App component
    add form

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,17 +5,19 @@ import Footer from './components/Footer'
 import Home from './pages/Home'
 import Gallery from './pages/Gallery'
 import ArtworkDetail from './pages/ArtworkDetail'
+import LinkPage from './pages/LinkPage'
 
 export default function App() {
   return (
     <>
-    <ScrollToTop />
+      <ScrollToTop />
       <Header />
       <main>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/galleria" element={<Gallery />} />
           <Route path="/opera/:id" element={<ArtworkDetail />} />
+          <Route path="/link" element={<LinkPage />} />
         </Routes>
       </main>
       <Footer />
